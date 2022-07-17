cc8d270958b5374dc94706563787192f0dfd433a
MrDomian <41548647+MrDomian@users.noreply.github.com>
2022-07-17 14:24:12 +0200

    Commit 3
    End course

---
+++ app19.js
@@ -0,0 +1,18 @@
+// Middleware
+
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+
+app.use(bodyparser.json());
+app.use((req,res,next)=>{
+    req.banana = 'banana';
+    next();
+});
+
+app.get('/',(req,res)=>{
+    console.log(req.banana);
+    res.send('Middleware');
+});
+
+app.listen(3000);
